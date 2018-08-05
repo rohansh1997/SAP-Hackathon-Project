4111fe374460a0c3d1079bcb6e2ef78f62b706a2
Rounak Arya <rounakarya30@gmail.com>
2018-08-05 20:32:12 +0530

    Added All Files

---
+++ webapp/controller/Page1.controller.js
@@ -0,0 +1,32 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.untitledProject.controller.Page1", {
+		handleRouteMatched: function(oEvent) {
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+				var oPath;
+				if (this.sContext) {
+					oPath = {
+						path: "/" + this.sContext,
+						parameters: oParams
+					};
+					this.getView().bindObject(oPath);
+				}
+			}
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("Page1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
+		}
+	});
+}, /* bExport= */ true);
